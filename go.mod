8a3f8e954d5e21a58599afecd3ad11fdf179ab3d
jamesespericueta <espericueta2303@gmail.com>
2020-05-24 21:19:34 -0400

    69 and 420 functionality

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jamesespericueta/bruh-bot
 
 go 1.14
 
-require github.com/bwmarrin/discordgo v0.20.3
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/bwmarrin/discordgo v0.20.3
+)
--- main.go
+++ main.go
@@ -4,27 +4,44 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	str "strings"
 	"syscall"
 
+	toml "github.com/BurntSushi/toml"
 	"github.com/bwmarrin/discordgo"
 )
 
 var counter int
+var numBruhs int
+
+type baseConfig struct {
+	Bruh bruhStruct
+}
+
+type bruhStruct struct {
+	Token string
+}
+type statsStruct struct {
+	BruhsServer1 int
+	BruhsServer2 int
+}
 
 func main() {
-	counter = 0
-	token := "Njg2Mzk1MjY1NzI0NDQ4ODAw.XsM3UQ.5VUK9psIpkG8ezxO5cBtcE9czKo"
 
-	dg, err := discordgo.New("Bot " + token)
+	counter = 0
+
+	dg, err := discordgo.New("Bot " + decodeConfig())
 
 	if err != nil {
-		fmt.Println("oh fuck we couldn't create the discord session,", err)
+		fmt.Println("Couldn't create discord session,", err)
 		return
 	}
-
+	stats := strconv.FormatInt(int64(decodeBruhStats()), 10)
+	fmt.Println(stats)
 	dg.AddHandler(messageCreate)
 
+	//just another error check
 	err = dg.Open()
 
 	if err != nil {
@@ -32,24 +49,72 @@ func main() {
 		return
 	}
 
-	fmt.Println("we vibin")
-
+	fmt.Println("Working")
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT, os.Interrupt, os.Kill)
 	<-c
 	dg.Close()
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	message := "there have been "
-	finalMessage := fmt.Sprintf("%s%d%s", message, counter, " bruhs")
+/*
+first store the
+*/
+func decodeBruhStats() int {
+	var bruhStats statsStruct
+	if _, err := toml.DecodeFile(".\\bruhStats.toml", &bruhStats); err != nil {
+		fmt.Println(err)
+	}
+	return bruhStats.BruhsServer2
+}
 
-	if m.Author.ID == s.State.User.ID {
+func decodeConfig() string {
+	var config baseConfig
+	if _, err := toml.DecodeFile(".\\config.toml", &config); err != nil {
+		fmt.Println(err)
+	}
+	return config.Bruh.Token
+}
+
+func presenceUpdate(s *discordgo.Session, m *discordgo.PresenceUpdate) {
+
+}
+
+/*
+updates the bruhs from the last time that the bot was shutdown
+*/
+func updateBruhs() int {
+	return 0
+}
+
+/*
+messageCreate simply in the event that a message is created
+*/
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	message := "there's been "
+	if m.Author.Bot {
 		return
 	} else if m.Content == "!bruh" {
+		finalMessage := fmt.Sprintf("%s%d%s", message, numBruhs, " bruhs")
+		if numBruhs == 1 {
+			finalMessage = fmt.Sprintf("%s%d%s", "there's been ", numBruhs, " bruh")
+		}
 		s.ChannelMessageSend(m.ChannelID, finalMessage)
+		fmt.Println(m.GuildID)
 	} else if str.Contains(m.Content, "bruh") {
-		//s.ChannelMessageSend(m.ChannelID, finalMessage)
-		counter++
+		numBruhs += str.Count(m.Content, "bruh")
+	}
+	if numBruhs == 69 {
+		s.ChannelMessageSend(m.Content, "69 bruhs, nice.")
+	}
+	if numBruhs == 420 {
+		s.ChannelMessageSend(m.Content, "420 bruhs, ahaha.")
+	}
+	if numBruhs == 420 {
+		s.ChannelMessageSend(m.Content, "69420 bruhs, type bruh again and I'll find you...")
 	}
 }
+
+/*TODO:
+create an array which stores the guildID and then a corresponding
+array which stores the amount of messages
+*/
